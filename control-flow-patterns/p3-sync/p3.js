5bef211c5b33d4eb8870a4f0f4b0172277eeae4a
d-smith <doug.smith.mail@gmail.com>
2018-08-14 10:27:27 -0700

    Added s3-sync pattern

---
+++ control-flow-patterns/p3-sync/p3.js
@@ -0,0 +1,19 @@
+module.exports.step1 = async (event, context, callback) => {
+    callback(null, 'step1');
+}
+
+module.exports.step2a = async (event, context, callback) => {
+    callback(null, 'step2a');
+}
+
+module.exports.step2b = async (event, context, callback) => {
+    callback(null, 'step2b');
+}
+
+module.exports.step3 = async (event, context, callback) => {
+    callback(null, 'step3');
+}
+
+module.exports.step4 = async (event, context, callback) => {
+    callback(null, 'step4');
+}

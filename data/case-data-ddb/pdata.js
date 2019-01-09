e565630460d694bd044218e1efb03d5ae0397c33
d-smith <doug.smith.mail@gmail.com>
2019-01-09 10:06:34 -0800

    Added retrieve of instance data to pdata

--- data/case-data-ddb/pdata.js
+++ data/case-data-ddb/pdata.js
@@ -6,8 +6,18 @@ const retrieve = async (event, context) => {
     let txnid = event.pathParameters.txnid;
     console.log(`retrieve data for transaction ${txnid}`);
 
-    
-    return {statusCode: 200, body: 'uh-huh'};
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    let params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            "TxnId": txnid
+        },
+        ConsistentRead: true
+    };
+
+    let rawData = await docClient.get(params).promise();
+
+    return { statusCode: 200, body: `${JSON.stringify(rawData)}` };
 
 };
 
